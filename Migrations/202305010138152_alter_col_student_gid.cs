29578fce983227782c5b14553677f0fcbcfbd9c9
林廣學 <tomkhlin@g.chu.edu.tw>
2023-05-02 11:14:17 +0800

    調整_DB欄位_將groups_GID改為GID 新增_StudentHomeViewModel

---
+++ Migrations/202305010138152_alter_col_student_gid.cs
@@ -0,0 +1,28 @@
+﻿namespace LMSweb.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class alter_col_student_gid : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Students", "group_GID", "dbo.Groups");
+            DropIndex("dbo.Students", new[] { "group_GID" });
+            RenameColumn(table: "dbo.Students", name: "group_GID", newName: "GID");
+            AlterColumn("dbo.Students", "GID", c => c.Int(nullable: true));
+            CreateIndex("dbo.Students", "GID");
+            AddForeignKey("dbo.Students", "GID", "dbo.Groups", "GID", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Students", "GID", "dbo.Groups");
+            DropIndex("dbo.Students", new[] { "GID" });
+            AlterColumn("dbo.Students", "GID", c => c.Int());
+            RenameColumn(table: "dbo.Students", name: "GID", newName: "group_GID");
+            CreateIndex("dbo.Students", "group_GID");
+            AddForeignKey("dbo.Students", "group_GID", "dbo.Groups", "GID");
+        }
+    }
+}
---
+++ ViewModel/StudentHomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMSweb.ViewModel
+{
+    public class StudentHomeViewModel
+    {
+        public string CourseID { get; set; }
+        public string CourseName { get; set; }
+        public string TeacherName { get; set; }
+        public string GroupName { get; set; }
+        public List<string> GroupMember { get; set; }
+    }
+}
