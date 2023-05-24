8d24fb62a107ffaf006ac8a91ee83f83c3790c7c
林廣學 <tomkhlin@g.chu.edu.tw>
2023-05-24 15:20:46 +0800

    更名_Execution中的CurrentAction改名為CurrentStatus

---
+++ Migrations/202305240718370_AlterColumnCurrentActionToCurrentStatus.cs
@@ -0,0 +1,20 @@
+﻿namespace LMSweb.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AlterColumnCurrentActionToCurrentStatus : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Executions", "CurrentStatus", c => c.String());
+            DropColumn("dbo.Executions", "CurrentAction");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Executions", "CurrentAction", c => c.String());
+            DropColumn("dbo.Executions", "CurrentStatus");
+        }
+    }
+}
