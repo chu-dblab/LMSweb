1fe1712ff052bad7d926ad18ca97672ab73ee42f
NickLu <word4205@gmail.com>
2023-06-15 10:59:39 +0800

    優化_新增Nav PV  與整合幾個舊業面

---
+++ Controllers/NavController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LMSweb.ViewModel.NavPartial;
+
+namespace LMSweb.Controllers
+{
+    public class NavController : Controller
+    {
+        // GET: Nav
+        public ActionResult _TeacherNavPartial(TeacherNavViewModel vm)
+        {
+            return PartialView(vm);
+        }
+    }
+}
---
+++ ViewModel/NavPartial/TeacherNavViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMSweb.ViewModel.NavPartial
+{
+    public class TeacherNavViewModel
+    {
+        public string CourseID { get; set; }
+    }
+}
